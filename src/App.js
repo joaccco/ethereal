2c5d381803a01ababe264547c1cfc80d7cc6aa98
joaccco <joaccofranc02@icloud.com>
2023-03-02 20:22:46 -0300

    caca

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Nav from './components/nav';
+import Footer from './components/footer';
+import Background from './components/background';
+import Slider from './components/slider';
+
+function App() {
+  return (
+    <div>
+    <Nav />
+    <Slider/>
+    <Background />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<<<<<<< HEAD
 
+=======
+>>>>>>> 5382413 (caca)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
