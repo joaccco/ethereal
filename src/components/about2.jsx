2b1f6ee77fa129572e46096f73d7786df006d792
joaccco <joaccofranc02@icloud.com>
2023-03-27 09:07:57 -0300

    Nuestras fotos

--- src/components/about2.jsx
+++ src/components/about2.jsx
@@ -7,7 +7,7 @@ class About2 extends Component {
             <div class="mt-8 grid grid-cols-2 gap-8 md:grid-cols-2 lg:grid-cols-4">
       
               <a
-                class="ml-10 block h-full w-3/4 rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+                class="ml-10 block h-full w-3/4 rounded-xl border border-gray-400 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-fuchsia-200/10"
                 href="/"
               >
               <img className='rounded-full w-4/5' src="/mateo.jpg"/>
@@ -15,7 +15,7 @@ class About2 extends Component {
               </a>
 
               <a
-                class="block h-full w-3/4 rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+                class="block h-full w-3/4 rounded-xl border border-gray-400 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-fuchsia-200/10"
                 href="/s"
               >
                 <img className='rounded-full w-4/5' src="/joaco.jpeg"/>
@@ -23,15 +23,15 @@ class About2 extends Component {
               </a>
 
               <a
-                class="block h-full w-4/5 rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+                class="block h-full w-4/5 rounded-xl border border-gray-400 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-fuchsia-200/10"
                 href="/"
               >
-                <img className='rounded-full w-4/5' src="/rodri.jpeg"/>
+                <img className='rounded-full w-4/5' src="/kevin.jpeg"/>
                 <h2 class="mt-4 text-xl font-bold text-white">Kevin Reyes</h2>
               </a>
 
               <a
-                class="block h-full w-3/4 rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+                class="block h-full w-3/4 rounded-xl border border-gray-400 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-fuchsia-200/10"
                 href="/"
               >
                 <img className='rounded-full w-4/5' src="/rodri.jpeg"/>
